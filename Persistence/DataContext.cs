c9faf1541a7428cb65d2e457eb215a6aeb685971
Hrvoje <travelagencyx@gmail.com>
2024-08-11 14:23:37 +0200

    End of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
