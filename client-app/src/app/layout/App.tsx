580e1c65f68d48e3db1b62af4b19144dc65c3ee5
Hrvoje <travelagencyx@gmail.com>
2025-02-04 15:28:17 +0100

    End of Section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,100 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './styles.css'
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent'
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 
 function App() {
-  const[activities, SetActivities] = useState<Activity[]>([]);
-  const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const[editMode, setEditMode] = useState(false);
-  const[loading, setLoading] = useState(true);
-  const[submitting, SetSubmitting] = useState(false);
+  const{activityStore} = useStore();
+
+//console.log(activityStore);
 
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activities: Activity[]= [];
-      response.forEach(activity =>{
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-        SetActivities(activities)
-        setLoading(false);
-      })
-  },[])
+    activityStore.loadActivities();
+  },[activityStore])
 
-  function handleSelectActivity(id:string){
-    setSelectedActivity(activities.find(x=>x.id === id));
-  }
+ 
 
-  function handleCancelSelectActivity(){
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?:string){
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose(){
-    setEditMode(false)
-  }
-
-  function handleCreateOrEditActivity(activity: Activity){
-    SetSubmitting(true);
-    if (activity.id){
-      agent.Activities.update(activity).then(() => {
-        SetActivities([...activities.filter(x=>x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        SetSubmitting(false);
-    })
-    }else{
-    activity.id=uuid();
-    agent.Activities.create(activity).then(()=>{
-      SetActivities([...activities, activity])
-      setSelectedActivity(activity);
-      setEditMode(false);
-      SetSubmitting(false);
-      })
-    }
-  }
-
-  function handleDeleteActivity(id:string){
-    SetSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      SetActivities([...activities.filter(x=>x.id !== id)])
-      SetSubmitting(false);
-    })
-  }
-
-  if (loading) return  <LoadingComponent content='Loading app..' />
+  if (activityStore.loadingInitial) return  <LoadingComponent content='Loading app..' />
   
   return (
     <>
-      <NavBar openForm={handleFormOpen}/>
+      <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-        activities={activities}
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelSelectActivity}
-        editMode={editMode}
-        openForm={handleFormOpen}
-        closeForm={handleFormClose}
-        createOrEdit={handleCreateOrEditActivity}
-        deleteActivity={handleDeleteActivity}
-        submitting={submitting}
-        />
-      </Container>
-      
+        <ActivityDashboard />
+      </Container>  
     </>
-  )
+  );
 }
 
-export default App
+export default observer(App);
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
 import App from './app/layout/App.tsx'
 import './app/layout/styles.css'
+import { store, StoreContext } from './app/stores/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  //<StrictMode>
+    <StoreContext.Provider value={store}>
     <App />
-  </StrictMode>,
+    </StoreContext.Provider>
+  //</StrictMode>,
 )
