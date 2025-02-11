0e1949e579fdc9c79f59ac8c3ae3c3190c1641ab
Hrvoje <travelagencyx@gmail.com>
2025-02-11 11:28:08 +0100

    End of Section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ export default class activityStore {
     selectedActivity: Activity | undefined = undefined;
     editMode=false;
     loading=false;
-    loadingInitial=true;
+    loadingInitial=false;
 
     constructor(){
         makeAutoObservable(this)
@@ -20,6 +20,7 @@ export default class activityStore {
     }
 
     loadActivities = async () =>{
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
             /*runInAction(() => {
@@ -29,10 +30,11 @@ export default class activityStore {
                     this.activities.push(activity);
                     })    
             })*/
+                runInAction(() => {
                 activities.forEach(activity =>{
                     activity.date = activity.date.split('T')[0];
                     this.activityRegistry.set(activity.id, activity);
-                    })  
+                    })})
                 this.setLoadingInitial(false);   
         } catch (error) {
             console.log(error);
@@ -40,27 +42,42 @@ export default class activityStore {
         }
     }
 
+    loadActivity = async (id:string) =>{
+        let activity=this.getActivity(id);
+        if (activity) {
+            this.selectedActivity=activity
+            return activity;}
+                else{
+            this.setLoadingInitial(true);
+            try {
+                activity= await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(()=>{
+                    this.selectedActivity=activity;
+                })
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+
+    }
+
+    private setActivity = (activity:Activity)=>{
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+    }
+
+    private getActivity = (id: string)=>{
+        return this.activityRegistry.get(id);
+    }
+
     setLoadingInitial = (state:boolean) =>{
         this.loadingInitial = state;
     }
     
-
-    selectActivity = (id:string) =>{
-        this.selectedActivity=this.activityRegistry.get(id);
-    }
-
-    cancelSelectedActivity = ()=>{
-        this.selectedActivity=undefined;
-    }
-
-    openForm = (id?:string)=>{
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode=true;
-    }
-
-    closeForm = ()=>{
-        this.editMode=false;
-    }
 
     createActivity = async (activity:Activity) =>{
         this.loading=true;
@@ -105,7 +122,6 @@ export default class activityStore {
             await agent.Activities.delete(id);
             runInAction(()=>{
                 this.activityRegistry.delete(id);
-                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading=false;
             })
 
